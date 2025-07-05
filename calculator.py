0064452de0a0629abc76efe20c20bc3417beda4a
Nemitha Prabashwara <137596737+Nemitha-prs@users.noreply.github.com>
2025-07-05 17:08:37 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,167 @@
+print('1.addition ')
+print('2.substraction ')
+print('3.multipication')
+print('4.division ')
+print('5.squared')
+print('6.to the power ')
+print('7.end ')
+print('8.reset')
+
+print('ex : if u need to multiply enter 3 .')
+op = int(input('enter the operation : '))
+
+                 
+                 
+if op == 1:
+    o = int(input('enter the first number : '))
+    s = int(input('enter the second number : '))
+    ans = o+s
+    print(o,' + ',s,'=',ans)
+    op = int(input('enter 8 to reset'))
+    
+           
+    
+    
+elif op == 2:
+    o = int(input('enter the first number : '))
+    s = int(input('enter the second number : '))
+    ans = o-s
+    print(o,' + ',s,'=',ans)
+    op = int(input('enter 8 to reset'))   
+    
+    
+elif op == 3:
+    o = int(input('enter the first number : '))
+    s = int(input('enter the second number : '))
+    ans = o*s
+    print(o,' x ',s,'=',ans)
+    op = int(input('enter 8 to reset'))
+    
+    
+    
+elif op == 4:
+    o = int(input('enter the first number : '))
+    s = int(input('enter the second number : '))
+    ans = o/s
+    print(o,' / ',s,'=',ans)
+    op = int(input('enter 8 to reset'))
+ 
+elif op == 5:
+    o = int(input('enter the number : '))
+    ans = o^2
+    print(o,'^2''=',ans)
+    op = int(input('enter 8 to reset'))
+    
+    
+elif op ==6:
+    o = int(input('enter the first number : '))
+    s = int(input('enter the second number : '))
+    ans = o^s
+    print(o,' ^ ',s,'=',ans)
+    op = int(input('enter 8 to reset'))
+    
+    
+elif op == 7:
+    print('thanks for using the app')
+
+while op == 8:
+    print('1.addition ')
+    print('2.substraction ')
+    print('3.multipication')
+    print('4.division ')
+    print('5.squared')
+    print('6.to the power ')
+    print('7.end ')
+    print('8.reset')
+
+    print('ex : if u need to multiply enter 3 .')
+    op = int(input('enter the operation : '))
+    
+    
+    if op == 1:
+        
+        o = int(input('enter the first number : '))
+        s = int(input('enter the second number : '))
+        ans = o+s
+        print(o,' + ',s,'=',ans)
+        op = int(input('enter 8 to reset'))
+    
+           
+    
+    
+    elif op == 2:
+       
+        o = int(input('enter the first number : '))
+        s = int(input('enter the second number : '))
+        ans = o-s
+        print(o,' + ',s,'=',ans)
+        op = int(input('enter 8 to reset'))
+    
+    
+    
+    elif op == 3:
+        
+        o = int(input('enter the first number : '))
+        s = int(input('enter the second number : '))
+        ans = o*s
+        print(o,' x ',s,'=',ans)
+        op = int(input('enter 8 to reset'))
+    
+    
+    
+    elif op == 4:
+        o = int(input('enter the first number : '))
+        s = int(input('enter the second number : '))
+        ans = o/s
+        print(o,' / ',s,'=',ans)
+        op = int(input('enter 8 to reset'))
+ 
+    elif op == 5:
+        o = int(input('enter the number : '))
+        ans = o^2
+        print(o,'^2''=',ans)
+        op = int(input('enter 8 to reset'))
+    
+    
+    elif op ==6:
+        o = int(input('enter the first number : '))
+        s = int(input('enter the second number : '))
+        ans = o^s
+        print(o,' ^ ',s,'=',ans)
+        op = int(input('enter 8 to reset'))
+    
+    
+    elif op == 7:
+        print('thanks for using the app')
+    
+      
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
